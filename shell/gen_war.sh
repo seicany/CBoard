b1f574117ad6b470069027c4ba458760a897b898
jihong.wang <jihong.wang@moji.com>
2017-07-02 19:15:02 +0800

    shell 脚本

--- shell/gen_war.sh
+++ shell/gen_war.sh
@@ -1,8 +1,8 @@
 # root 账户执行
 
 cd /opt/wjhapp/cboard/CBoard
-mvn package && mv target/cboard.war ROOT.war
-mv /opt/wjhapp/cboard/CBoard/ROOT.war /usr/local/tomcat/apache-tomcat-8.5.16/webapps/
+mvn package && mv target/cboard.war target/ROOT.war
+cp /opt/wjhapp/cboard/CBoard/target/ROOT.war /usr/local/tomcat/apache-tomcat-8.5.16/webapps/
 
 cd /usr/local/tomcat/apache-tomcat-8.5.16
 ./bin/shutdown.sh && ./bin/startup.sh
