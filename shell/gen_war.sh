947d32b1bb840165b7618895905b4f9dbfbcc801
jihong.wang <jihong.wang@moji.com>
2017-07-02 12:50:00 +0800

    shell 脚本

--- shell/gen_war.sh
+++ shell/gen_war.sh
@@ -1,2 +1,10 @@
+# root 账户执行
+
 cd /opt/wjhapp/cboard/CBoard
-jar cvf CBoard.war  *.*/  /usr/local/tomcat/apache-tomcat-8.5.16/webapps/
+jar cvf ROOT.war  *.*/
+mv /opt/wjhapp/cboard/CBoard/ROOT.war /usr/local/tomcat/apache-tomcat-8.5.16/webapps/
+
+cd /usr/local/tomcat/apache-tomcat-8.5.16/bin
+./shutdown.sh && ./startup.sh
+
+ps -ef|grep tomcat
